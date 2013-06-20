8f74146b47366174a9dc313acac58fa467329f26
justwrote <justwrote@gmail.com>
2013-06-20 10:23:09 +0200

    bumped all versions

--- project/build.scala
+++ project/build.scala
@@ -1,27 +1,21 @@
 import sbt._
 import Keys._
-import sbtrelease.Release._
+import sbtrelease.ReleasePlugin._
 import ls.Plugin.LsKeys
 
-object Versions {
-  val Scala_2_10 = "2.10.0"
-  val ScalaTest_2_10 = "1.9.1"
-}
-
 object Dependencies {
-  import Versions._
 
-  val snakeYAML = "org.yaml" % "snakeyaml" % "1.9"
+  val snakeYAML = "org.yaml" % "snakeyaml" % "1.12"
 
   def scalatest(version: String) = {
     def st(scalaVersion: String, projectVersion: String) =
       "org.scalatest" % ("scalatest_" + scalaVersion) % projectVersion % "test"
 
     version match {
-      case "2.8.0" => st("2.8.1", "1.5.1") // argh, there is no 2.8.0 scalatest version in any maven repository
-      case "2.8.1" | "2.8.2" => st(version, "1.5.1")
-      case v if v.startsWith("2.9.")  => st(version, "1.6.1")
-      case Scala_2_10 => st("2.10", ScalaTest_2_10)
+      case "2.8.0" => st("2.8.1", "1.8") // argh, there is no 2.8.0 scalatest version in any maven repository
+      case "2.8.1" | "2.8.2" => st(version, "1.8")
+      case v if v.startsWith("2.9.")  => st(version, "1.9.1")
+      case v if v.startsWith("2.10.") => st("2.10", "1.9.1")
       case _ => sys.error("ScalaTest not supported for scala version %s!" format version)
     }
   }
@@ -31,16 +25,15 @@ object Dependencies {
 }
 
 object ScalaFaker extends Build {
-  import Versions._
 
   def sourceDir(version: String) = version match {
     case "2.8.0" => "-2.8.0"
-    case Scala_2_10 => "-2.10"
+    case v if v.startsWith("2.10.") => "-2.10"
     case _ => ""
   }
 
   def additionalCompilerOptions(version: String): Seq[String] = version match {
-    case Scala_2_10 => Seq("-feature")
+    case v if v.startsWith("2.10.") => Seq("-feature")
     case _ => Seq.empty
   }
 
@@ -52,7 +45,7 @@ object ScalaFaker extends Build {
       organization := "it.justwrote",
       name := projectName,
       scalaVersion := "2.9.2",
-      crossScalaVersions := Seq("2.8.0", "2.8.1", "2.8.2", "2.9.0", "2.9.0-1", "2.9.1", "2.9.2", Scala_2_10),
+      crossScalaVersions := Seq("2.8.0", "2.8.1", "2.8.2", "2.9.0", "2.9.0-1", "2.9.1", "2.9.2", "2.10.2"),
       publishArtifact in (Compile, packageDoc) := false,
       scalacOptions ++= Seq("-deprecation", "-Xcheckinit", "-encoding", "utf8", "-g:vars", "-unchecked", "-optimize"),
       parallelExecution := true,
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,6 +1,6 @@
 resolvers += "gseitz@github" at "http://gseitz.github.com/maven/"
 
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.4")
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.7")
 
 resolvers ++= Seq(
   "lessis" at "http://repo.lessis.me",
@@ -8,3 +8,7 @@ resolvers ++= Seq(
 )
 
 addSbtPlugin("me.lessis" % "ls-sbt" % "0.1.2")
+
+addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.4.0")
+
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.1")
